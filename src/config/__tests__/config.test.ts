fce9b14c8d760123c8a029484b6d7c958a3772f8
Alexey Vasyukov <vasyukov.alexey.v@gmail.com>
2019-08-11 14:35:47 +0300

    Some tunes

--- src/config/__tests__/config.test.ts
+++ src/config/__tests__/config.test.ts
@@ -1,19 +1,23 @@
-import { load, save } from '../../config';
+import { load, make, save } from '../../config';
+import { Core } from '../../interfaces';
 
 describe('load', () => {
   it('should load config', async () => {
-    const config = await load();
-    expect(config);
+    const config = await load('');
+    expect(config).toBe(true);
   });
 });
 
 describe('save', () => {
   it('should save config', async () => {
-    const config = await save();
-    expect(config);
+    const config = await save([{} as Core.Pack]);
+    expect(config).toBe(true);
   });
 });
 
 describe('make', () => {
-  it('should make config for path', () => {});
+  it('should make config for path', async () => {
+    const config = await make('');
+    expect(config).toBe(true);
+  });
 });
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,7 @@
-export async function load() {}
+import { Core } from '../interfaces';
 
-export async function save() {}
+export async function load(_path: string) {}
+
+export async function save(_config: Core.Pack[]) {}
+
+export async function make(_path: string) {}
