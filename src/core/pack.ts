eaf110ef1daae1379305a843de22643cf20fcd77
Alexey Vasyukov <vasyukov.alexey.v@gmail.com>
2019-08-15 09:38:45 +0300

    Updated Core.setVersion

--- src/core/pack.ts
+++ src/core/pack.ts
@@ -1,13 +1,12 @@
 import { Core } from '../interfaces';
 
 export async function setVersion(pack: Core.Pack, version: string): Promise<Core.Pack> {
-  if (!pack.availableVersions.includes(version)) {
+  if (!pack.availableVersions || !pack.availableVersions.includes(version)) {
     throw new Error('Version does not exists.');
   }
 
-  for (const file of pack.files) {
-    // TODO: Add parallel set file version
-    await setFileVersion(file, version);
+  if (pack.files && pack.files.length) {
+    await Promise.all(pack.files.map(file => setFileVersion(file, version)));
   }
 
   return {
