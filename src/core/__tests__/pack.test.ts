9b2e322e7674e0be85b1df7c08c32dccd5490140
Alexey Vasyukov <vasyukov.alexey.v@gmail.com>
2019-08-24 11:29:27 +0300

    Added fixtures for setFileVersion

--- src/core/__tests__/pack.test.ts
+++ src/core/__tests__/pack.test.ts
@@ -29,3 +29,9 @@ describe('setVersion', () => {
       .toEqual(new Error('Version does not exists.'));
   });
 });
+
+describe('setFileVersion', () => {
+  it('should switch version of file', () => {
+
+  });
+});
